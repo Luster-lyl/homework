d61770a52101c3e910c3ae2b08d22f2f96ad63d6
Luster-lyl <619281245@qq.com>
2019-10-29 13:33:00 +0800

    3

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,16 +2,29 @@ import React, {Component} from 'react'
 
 import { Form, Icon, Input, Button, Checkbox,Layout } from 'antd'
 
+import './login.css';
+
+var loginbg = {
+    width:"100%",
+    height:"1080px"
+};
 
 const { Header, Footer, Sider, Content } = Layout
 class LogIn extends Component{
     render(){
         return (
-            <div>
+            <div style={loginbg} className="loginBg">
                 <Layout>
-                <Header>Header</Header>
-                <Content>Content-login</Content>
-                 <Footer>Footer</Footer>
+                <Header></Header>
+                <Content>
+                    <img src={require("../../assets/imgs/index-bg.png")}></img>
+                    <p>登录</p>
+                    <div className="loginForm">
+                        <span className="loginAccount">邮箱/手机号<input type="text"></input></span>
+                        <span className="password">密&#12288;&#12288;&#12288;&nbsp;码<input type="text"></input></span>
+                    </div>
+                </Content>
+                 <Footer></Footer>
                 </Layout>
             </div>
         )
